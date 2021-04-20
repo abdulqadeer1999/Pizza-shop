352f8748f2b6ed1cb912be76da639f28946ac0fc
abdulqadeer1999 <abdulqadeersolangi007@gmail.com>
2021-04-20 22:27:37 +0500

    changes

--- src/components/Prices.js
+++ src/components/Prices.js
@@ -1,4 +1,10 @@
 
+import Pizza from "../images/pizza1.jpg"
+import Pizza2 from "../images/pizza5.jpg"
+import Pizza3 from "../images/pizza6.jfif"
+import Pizza4 from "../images/pizza7.jfif"
+import Pizza5 from "../images/uspizza.jpg"
+import Pizza6 from "../images/american.jpg"
 const Prices = () => {
     return (
         <div className="prices">
@@ -7,7 +13,96 @@ const Prices = () => {
        <div className="col-4">
            <div className="price">
              <div className="price__img">
-
+            <img src={Pizza}  alt="pizza" />
+             </div>
+             <div className="price__heading">
+             <h1 className="price__heading">
+                 Pizza Margerita
+             </h1>
+             <p className="price__text">
+                 lo j k o o  h h j  i i n n jn n n   j j j j j j j
+             </p>
+             <p className="price_rs">$50</p>
+             </div>
+           </div>
+       </div>
+       <div className="col-4">
+           <div className="price">
+             <div className="price__img">
+            <img src={Pizza2}  alt="pizza" />
+             </div>
+             <div className="price__heading">
+             <h1 className="price__heading">
+                 Pizza Margerita
+             </h1>
+             <p className="price__text">
+                 lo j k o o  h h j  i i n n jn n n   j j j j j j j
+             </p>
+             <p className="price_rs">$50</p>
+             </div>
+           </div>
+       </div>
+       <div className="col-4">
+           <div className="price">
+             <div className="price__img">
+            <img src={Pizza3}  alt="pizza" />
+             </div>
+             <div className="price__heading">
+             <h1 className="price__heading">
+                 Pizza Margerita
+             </h1>
+             <p className="price__text">
+                 lo j k o o  h h j  i i n n jn n n   j j j j j j j
+             </p>
+             <p className="price_rs">$50</p>
+             </div>
+           </div>
+       </div>
+       <div className="col-4">
+           <div className="price">
+             <div className="price__img">
+            <img src={Pizza4}  alt="pizza" />
+             </div>
+             <div className="price__heading">
+             <h1 className="price__heading">
+                 Pizza Margerita
+             </h1>
+             <p className="price__text">
+                 lo j k o o  h h j  i i n n jn n n   j j j j j j j
+             </p>
+             <p className="price_rs">$50</p>
+             </div>
+           </div>
+       </div>
+       <div className="col-4">
+           <div className="price">
+             <div className="price__img">
+            <img src={Pizza5}  alt="pizza" />
+             </div>
+             <div className="price__heading">
+             <h1 className="price__heading">
+                 Pizza Margerita
+             </h1>
+             <p className="price__text">
+                 lo j k o o  h h j  i i n n jn n n   j j j j j j j
+             </p>
+             <p className="price_rs">$50</p>
+             </div>
+           </div>
+       </div>
+       <div className="col-4">
+           <div className="price">
+             <div className="price__img">
+            <img src={Pizza6}  alt="pizza" />
+             </div>
+             <div className="price__heading">
+             <h1 className="price__heading">
+                 Pizza Margerita
+             </h1>
+             <p className="price__text">
+                 lo j k o o  h h j  i i n n jn n n   j j j j j j j
+             </p>
+             <p className="price_rs">$50</p>
              </div>
            </div>
        </div>
